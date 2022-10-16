66f6f9a33dce54950eba656912bb797b0827a2f3
Artur Meinzer <artur.meinzer@web.de>
2022-10-16 13:26:08 +0200

    Fix unused import

--- components/Artist.jsx
+++ components/Artist.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import Image from "next/image";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
